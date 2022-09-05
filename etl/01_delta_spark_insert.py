8519c3c1fab9607243c2cf59c860766bdf5d39fc
mesaquejunior <mesaquejunior@gmail.com>
2022-09-05 21:49:59 +0100

    configuring delta lab

---
+++ etl/01_delta_spark_insert.py
@@ -0,0 +1,33 @@
+from delta.tables import *
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, min, max
+
+# Cria objeto da Spark Session
+spark = (SparkSession.builder.appName("DeltaExercise")
+         .config("spark.jars.packages", "io.delta:delta-core_2.12:1.0.0")
+         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+         .getOrCreate()
+         )
+
+# Importa o modulo das tabelas delta
+
+# Leitura de dados
+enem = (
+    spark.read.format("csv")
+    .option("inferSchema", True)
+    .option("header", True)
+    .option("delimiter", ";")
+    .load("s3://datalake-igti-mesaque/raw-data/enem/")
+)
+
+# Escreve a tabela em staging em formato delta
+print("Writing delta table...")
+(
+    enem
+    .write
+    .mode("overwrite")
+    .format("delta")
+    .partitionBy("year")
+    .save("s3://datalake-igti-mesaque/staging-zone/enem")
+)
--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -6,3 +6,19 @@ resource "aws_s3_object" "codigo_spark" {
   acl    = "private"
   etag   = filemd5("../src/job_spark.py")
 }
+
+resource "aws_s3_object" "delta_insert" {
+  bucket = aws_s3_bucket.data_lake.id
+  key    = "emr-code/pyspark/01_delta_spark_insert.py"
+  source = "../etl/01_delta_spark_insert.py"
+  acl    = "private"
+  etag   = filemd5("../etl/01_delta_spark_insert.py")
+}
+
+resource "aws_s3_object" "delta_upsert" {
+  bucket = aws_s3_bucket.data_lake.id
+  key    = "emr-code/pyspark/02_delta_spark_upsert.py"
+  source = "../etl/02_delta_spark_upsert.py"
+  acl    = "private"
+  etag   = filemd5("../etl/02_delta_spark_upsert.py")
+}
